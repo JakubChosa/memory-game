e3a9d32f11cead30d303a457d49386f4a60c44ef
Jakub <kuba.chosa@gmail.com>
2022-09-27 13:55:01 +0100

    app init

---
+++ index.js
@@ -0,0 +1,105 @@
+const section = document.querySelector("section");
+const attemptsCount = document.querySelector(".attempts");
+const resetBtn = document.getElementById("button");
+let attempts = 0;
+let currentScore = 0;
+
+attemptsCount.textContent = `Attempts: ${attempts}`;
+
+const getData = () => [
+  { imgSrc: "./images/baseball.jpg", name: "baseball" },
+  { imgSrc: "./images/basketball.jpg", name: "basketball" },
+  { imgSrc: "./images/cycling.jpg", name: "cycling" },
+  { imgSrc: "./images/running.jpg", name: "running" },
+  { imgSrc: "./images/skiing.jpg", name: "skiing" },
+  { imgSrc: "./images/swimming.jpg", name: "swimming" },
+  { imgSrc: "./images/table-tennis.jpg", name: "table-tennis" },
+  { imgSrc: "./images/voleyball.jpg", name: "voleyball" },
+  { imgSrc: "./images/baseball.jpg", name: "baseball" },
+  { imgSrc: "./images/basketball.jpg", name: "basketball" },
+  { imgSrc: "./images/cycling.jpg", name: "cycling" },
+  { imgSrc: "./images/running.jpg", name: "running" },
+  { imgSrc: "./images/skiing.jpg", name: "skiing" },
+  { imgSrc: "./images/swimming.jpg", name: "swimming" },
+  { imgSrc: "./images/table-tennis.jpg", name: "table-tennis" },
+  { imgSrc: "./images/voleyball.jpg", name: "voleyball" },
+];
+const randomizeData = () => {
+  const data = getData();
+  data.sort(() => Math.random() - 0.5);
+  return data;
+};
+
+const cardGenerator = () => {
+  const cardData = randomizeData();
+  cardData.forEach((item) => {
+    const card = document.createElement("div");
+    const cardFace = document.createElement("img");
+    cardFace.src = item.imgSrc;
+    const cardBack = document.createElement("div");
+    card.classList = "card";
+    cardFace.classList = "card-face";
+    cardBack.classList = "card-back";
+    section.append(card);
+    card.append(cardFace);
+    card.append(cardBack);
+    card.setAttribute("name", item.name);
+    card.addEventListener("click", (e) => {
+      card.classList.toggle("toggle-card");
+      checkCards(e);
+    });
+  });
+};
+cardGenerator();
+const checkCards = (e) => {
+  const clickedCard = e.target;
+  clickedCard.classList.add("flipped");
+  const flippedCards = document.querySelectorAll(".flipped");
+  if (flippedCards.length === 2) {
+    if (
+      flippedCards[0].getAttribute("name") ===
+      flippedCards[1].getAttribute("name")
+    ) {
+      flippedCards.forEach((card) => {
+        card.classList.remove("flipped");
+        card.style.pointerEvents = "none";
+      });
+      attempts++;
+    } else {
+      flippedCards.forEach((card) => {
+        card.classList.remove("flipped");
+        setTimeout(() => card.classList.remove("toggle-card"), 1000);
+      });
+      attempts++;
+      setTimeout(
+        () => (attemptsCount.textContent = `Attempts: ${attempts}`),
+        1000
+      );
+    }
+  }
+  const toggleCards = document.querySelectorAll(".toggle-card");
+  if (toggleCards.length === 16) {
+    currentScore = attempts;
+    restart();
+    attemptsCount.textContent = `You won with ${currentScore} attempts`;
+  }
+};
+
+const restart = () => {
+  attemptsCount.textContent = `Attempts${attempts}`;
+  section.style.pointerEvents = "none";
+  let cardData = randomizeData();
+  let cards = document.querySelectorAll(".card");
+  let faces = document.querySelectorAll(".card-face");
+  cardData.forEach((card, index) => {
+    cards[index].classList.remove("toggle-card");
+    cards[index].setAttribute("name", card.name);
+    cards[index].style.pointerEvents = "all";
+    setTimeout(() => (faces[index].src = card.imgSrc), 1000);
+  });
+  attempts = 0;
+  attemptsCount.textContent = attempts;
+  setTimeout(() => (section.style.pointerEvents = "all"), 1000);
+};
+
+resetBtn.addEventListener("click", restart);
